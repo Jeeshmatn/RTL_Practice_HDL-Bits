6acf6f2d73dfa0c7b0a485ca137a3a9db4afbf34
jeeshma tn <114424425+Jeeshmatn@users.noreply.github.com>
2025-05-27 15:59:49 +0530

    formatted

--- Vectors/vector part select.v
+++ Vectors/vector part select.v
@@ -6,10 +6,10 @@ module top_module(
     input [31:0] in,
     output [31:0] out );
 
-assign [31:24] =   in[7:0];
-assign [23:16] =   in[15:8];
-assign [15:8]  =   in[23:16];
-assign [7:0]   =   in[31:24];
+        assign [31:24] =   in[7:0];
+        assign [23:16] =   in[15:8];
+        assign [15:8]  =   in[23:16];
+        assign [7:0]   =   in[31:24];
 
 
 endmodule
